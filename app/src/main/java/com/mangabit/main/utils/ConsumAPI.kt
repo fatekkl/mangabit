72184fab88519743f2aca012bb36cb4cc0524b01
fatekkl <mathtml.1105@gmail.com>
2024-04-25 12:11:05 -0300

    create getData & parseJson functions, get from API data

---
+++ app/src/main/java/com/mangabit/main/utils/ConsumAPI.kt
@@ -0,0 +1,40 @@
+package com.mangabit.main.utils
+
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.mangabit.main.model.MangaParser
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
+fun getData():String?{
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+        .url("https://api.jikan.moe/v4/random/manga")
+        .build()
+
+
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) {
+            throw Exception("Error: $response")
+        }
+
+        return response.body?.string()
+    }
+}
+
+
+
+fun parseJson (jsonString: String?): MangaParser {
+    val gson = Gson()
+
+    val jsonObject = JsonParser.parseString(jsonString).asJsonObject
+
+    val data = jsonObject.getAsJsonObject("data")
+
+    return gson.fromJson(data , MangaParser::class.java)
+}
+
+
+
