60cfdc52d2e67e0f9c367ea86bcd8620722924a4
fatekkl <mathtml.1105@gmail.com>
2024-04-27 15:47:09 -0300

    att class manga parser

--- app/src/main/java/com/mangabit/main/model/MangaParser.kt
+++ app/src/main/java/com/mangabit/main/model/MangaParser.kt
@@ -1,5 +1,6 @@
 package com.mangabit.main.model
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 
 
@@ -7,12 +8,34 @@ data class MangaParser(
     val mal_id: Int,
     val url: String,
     val images: JsonObject,
-    val name: String,
-    val name_kanji: String,
-    val nicknames: List<String>,
-    val favorites: Int,
-    val about: String,
-){
+    val approved: Boolean,
+    val titles: JsonArray,
+    val title: String,
+    val title_english: String,
+    val title_japanese: String,
+    val type: String = "Manga",
+    val chapters: Int,
+    val volumes: Int,
+    val status: String,
+    val publishing: Boolean,
+    val published: JsonObject,
+    val score: Int,
+    val scored_by: Int,
+    val rank: Int,
+    val popularity: Int,
+    val members: Int,
+    val synopsis: String,
+    val background: String,
+    val authors: JsonArray,
+    val serializations: JsonArray,
+    val genres: JsonArray,
+    val explicit_genres: JsonArray,
+    val themes: JsonArray,
+    val demographics: JsonArray
 
+) {
 
+    fun showManga() {
+        println(title)
+    }
 }
