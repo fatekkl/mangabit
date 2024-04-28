09ce29f79a729fccd2dbedbaa85f447b61a95f4e
fatekkl <mathtml.1105@gmail.com>
2024-04-28 13:52:58 -0300

    upd getdata

--- app/src/main/java/com/mangabit/main/utils/ConsumAPI.kt
+++ app/src/main/java/com/mangabit/main/utils/ConsumAPI.kt
@@ -5,35 +5,38 @@ import com.google.gson.JsonParser
 import com.mangabit.main.model.MangaParser
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.IOException
 
 
-private fun getData():String?{
+private fun getData(): String? {
     val client = OkHttpClient()
 
     val request = Request.Builder()
         .url("https://api.jikan.moe/v4/top/manga")
         .build()
 
-
-    client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful) {
-            throw Exception("Error: $response")
+    return  try {
+        client.newCall(request).execute().use { response ->
+            if(!response.isSuccessful) {
+                throw IOException("Error: $response")
+            }
+            response.body?.string()
         }
-
-        return response.body?.string()
+    } catch (e: IOException) {
+        e.printStackTrace()
+        null
     }
 }
 
 
-
-private fun parseJson (jsonString: String?): List<MangaParser> {
+private fun parseJson(jsonString: String?): List<MangaParser> {
     val gson = Gson()
 
     val jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
     val data = jsonObject.getAsJsonArray("data")
 
-    return gson.fromJson(data , Array<MangaParser>::class.java).toList()
+    return gson.fromJson(data, Array<MangaParser>::class.java).toList()
 }
 
 
